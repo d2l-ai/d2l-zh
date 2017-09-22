30ca4c5792d8b1de22cc9ea5b9bf35c6bf0c5ff4
Mu Li <mli@amazon.com>
2017-09-22 03:20:42 +0000

    enable alexnet

--- sphinx_plugin.py
+++ sphinx_plugin.py
@@ -18,7 +18,7 @@ timeout = 300
 # limit the number of lines in a cell output
 max_output_length = 500
 # the files will be ingored for execution
-ignore_execution = ['alexnet']
+ignore_execution = []
 
 def _replace_ext(fname, new_ext):
     """replace the file extension in a filename"""
