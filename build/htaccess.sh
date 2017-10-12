5fec8abc457903b36620fa909ddc6d7ab71316e5
Mu Li <mli@amazon.com>
2017-10-12 04:57:50 +0000

    fix access

--- build/htaccess.sh
+++ build/htaccess.sh
@@ -6,7 +6,7 @@ OUT=.htaccess
 echo "ErrorDocument 404 https://zh.gluon.ai/404.html" >$OUT
 echo "RewriteEngine On" >>$OUT
 echo "RewriteCond %{SERVER_PORT} 80" >>$OUT
-echo "RewriteRule ^(.*)$ https://zh.gluon.ai/$1 [R,L]" >>$OUT
+echo "RewriteRule ^(.*)$ https://zh.gluon.ai/\$1 [R,L]" >>$OUT
 for f in chapter*/*; do
     if [[ $f == *"index"* ]]; then
         continue
