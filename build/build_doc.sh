48ac8c83f38d64fec6d4cecedfff44ec59083f04
Aston Zhang <asv325@gmail.com>
2018-05-25 00:48:44 +0000

    fix jenkins

--- build/build_doc.sh
+++ build/build_doc.sh
@@ -27,7 +27,11 @@ cp build/_build/latex/gluon_tutorials_zh.pdf build/_build/html/
 [ -e build/_build/latex/gluon_tutorials_zh.aux ] && rm build/_build/latex/gluon_tutorials_zh.aux
 [ -e build/_build/latex/gluon_tutorials_zh.idx ] && rm build/_build/latex/gluon_tutorials_zh.idx
 
-bash build/ipynb2mdd.sh
-cp build/mdd.zip build/_build/html/mdd.zip
+cd build
+
+bash ipynb2mdd.sh
+cp mdd.zip _build/html/mdd.zip
+
+cd ..
 
 aws s3 sync --delete build/_build/html/ s3://zh.gluon.ai/ --acl public-read
