e0c4fd4c9d92bc1ebe26c0b6e3d85e92409ecdd0
Aston Zhang <asv325@gmail.com>
2018-05-25 01:29:56 +0000

    improve mdd

--- build/ipynb2mdd.sh
+++ build/ipynb2mdd.sh
@@ -5,15 +5,19 @@ mkdir $MD
 
 cp index.md $MD/
 cp -R img $MD/
-for d in chapter*; do
-	cp -R $d $MD/
+for f in chapter*/*; do
+	dir=$(dirname "$f")
+	if [ "${f##*.}" = "md" ] || [ "${f##*.}" = "ipynb" ]; then
+		mkdir -p $MD/$dir
+		cp $f $MD/$f
+	fi
 done
 
 for f in $MD/chapter*/*ipynb; do
-	base=$(basename $f)
-	jupyter nbconvert --to markdown $f --output "${base%%.*}.md" 
-	rm $f
+    base=$(basename $f)
+    jupyter nbconvert --to markdown $f --output "${base%%.*}.md" 
+    rm $f
 done
 
-zip -r "$MD.zip" $MD
-[ -e $MD ] && rm -rf $MD
+zip -r "$MD.zip" $MD 
+[ -e $MD ] && rm -rf $MD 
