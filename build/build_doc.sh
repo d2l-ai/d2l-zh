1bf5cc80e4f97c1830c95578d32198a602ddef16
Aston Zhang <asv325@gmail.com>
2018-06-15 00:30:43 +0000

    update

--- build/build_doc.sh
+++ build/build_doc.sh
@@ -7,16 +7,17 @@ set -e
 # Clean build/chapter*/*ipynb and build/chapter*/*md that are no longer needed.
 for ch in chapter*; do
     if ! [ -e "../$ch" ]; then
-        [ -e $ch ] && rm -rf $ch
-    fi  
-    for f in $ch/*.md $ch/*.ipynb; do
-        base=$(basename $f)
-        md=${base%%.*}.md
-        echo "../$ch/$md"
-        if ! [ -e "../$ch/$md" ]; then
-            [ -e $f ] && rm $f
-        fi  
-    done
+        rm -rf $ch
+	else
+		shopt -s nullglob
+		for f in $ch/*.md $ch/*.ipynb; do
+			base=$(basename $f)
+			md=${base%%.*}.md
+			if ! [ -e "../$ch/$md" ]; then
+				rm $f
+			fi
+		done
+	fi
 done
 
 # prepare the env
