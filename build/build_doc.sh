24aa5da0ebca9aaaf385e6cf502a6b621214cc4d
Aston Zhang <asv325@gmail.com>
2018-06-15 00:58:04 +0000

    test

--- build/build_doc.sh
+++ build/build_doc.sh
@@ -7,18 +7,22 @@ set -e
 # Clean build/chapter*/*ipynb and build/chapter*/*md that are no longer needed.
 for ch in chapter*; do
     if ! [ -e "../$ch" ]; then
-        rm -rf $ch
-	else
-		shopt -s nullglob
-		for f in $ch/*.md $ch/*.ipynb; do
-			base=$(basename $f)
-			md=${base%%.*}.md
-			if ! [ -e "../$ch/$md" ]; then
-				rm $f
-			fi
-		done
-	fi
+        rm -rf $ch 
+    else
+        shopt -s nullglob
+        for f in $ch/*.md $ch/*.ipynb; do
+            echo $f
+            base=$(basename $f)
+            md=${base%%.*}.md
+            if ! [ -e "../$ch/$md" ]; then
+                rm $f
+            fi  
+        done
+    fi  
 done
+
+ls chapter*
+
 
 # prepare the env
 conda env update -f build/build.yml
