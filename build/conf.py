e47f3b0294192a2b12eb99a3c2aa13b4ffe6087c
Aston Zhang <asv325@gmail.com>
2018-07-23 04:40:32 +0000

    copyright 2017--2018

--- build/conf.py
+++ build/conf.py
@@ -73,7 +73,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = '动手学深度学习'
-copyright = '2017, Contributors'
+copyright = '2017--2018, Contributors'
 author = "MXNet Community"
 
 
