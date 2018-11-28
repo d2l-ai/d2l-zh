5230b78409ba94d25e135957be1029aac9776383
Aston Zhang <22279212+astonzhang@users.noreply.github.com>
2018-11-28 01:19:13 -0800

    Update build_pkg.sh

--- build/build_pkg.sh
+++ build/build_pkg.sh
@@ -7,3 +7,6 @@ make pkg
 # backup build/data to avoid download the dataset each time and put the
 rm -rf build/data
 mv build/data-bak build/data
+
+# For 1.0
+cp build/_build/html/d2l-zh.zip build/_build/html/d2l-zh-1.0.zip
