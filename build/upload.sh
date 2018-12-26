bbe0c99ceca408b4548b02e81303aa09e7e040d5
Mu Li <muli@cs.cmu.edu>
2018-12-26 15:50:58 -0800

    temperately upload into two s3 buckets

--- build/upload.sh
+++ build/upload.sh
@@ -3,10 +3,13 @@ set -e
 
 conda activate d2l-zh-build
 
-BUCKET=s3://zh.diveintodeeplearning.org
+# BUCKET=s3://zh.diveintodeeplearning.org
 # BUCKET=s3://diveintodeeplearning-staging
 
 DIR=build/_build/html/
+
+aws s3 sync --delete $DIR s3://zh.d2l.ai --acl 'public-read' --quiet
+aws s3 sync --delete $DIR s3://zh.diveintodeeplearning.org --acl 'public-read' --quiet
 
 #find $DIR \( -iname '*.css' -o -iname '*.js' \) -exec gzip -9 -n {} \; -exec mv {}.gz {} \;
 
@@ -16,10 +19,10 @@ DIR=build/_build/html/
 #      --acl 'public-read' \
 #      $DIR $BUCKET
 
-aws s3 sync --exclude '*.*' --include '*.woff' --include '*.woff2' \
-      --expires "$(date -d '+24 months' --utc +'%Y-%m-%dT%H:%M:%SZ')" \
-      --acl 'public-read' --quiet \
-      $DIR $BUCKET
+#aws s3 sync --exclude '*.*' --include '*.woff' --include '*.woff2' \
+#      --expires "$(date -d '+24 months' --utc +'%Y-%m-%dT%H:%M:%SZ')" \
+#      --acl 'public-read' --quiet \
+#      $DIR $BUCKET
 
 #aws s3 sync --exclude '*.*' --include '*.js' \
 #      --content-type 'application/javascript' \
@@ -27,4 +30,4 @@ aws s3 sync --exclude '*.*' --include '*.woff' --include '*.woff2' \
 #      --acl 'public-read' \
 #      $DIR $BUCKET
 
-aws s3 sync --delete $DIR $BUCKET --acl 'public-read' --quiet
+#aws s3 sync --delete $DIR $BUCKET --acl 'public-read' --quiet
