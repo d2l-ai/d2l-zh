8df6addfb970db7b189114b7c411166f5a54cefe
Aston Zhang <asv325@gmail.com>
2019-01-09 03:29:47 +0000

    rm logo

--- build/conf.py
+++ build/conf.py
@@ -312,7 +312,7 @@ latex_documents = [
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
-latex_logo = '_static/gluon.png'
+#latex_logo = '_static/gluon.png'
 
 # latex_engine  = 'xelatex'
 # For "manual" documents, if this is true, then toplevel headings are parts,
