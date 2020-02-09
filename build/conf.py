1ed0d05a8bca12d67886c6370af0c8e35c646a41
Aston Zhang <astonz@amazon.com>
2020-02-08 16:44:35 -0800

    add logo and favicon

--- build/conf.py
+++ build/conf.py
@@ -171,12 +171,12 @@ html_theme_options = {
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-# html_logo = '_static/gluon_white.png'
+html_logo = '_static/logo-with-text.png'
 
 # The name of an image file (relative to this directory) to use as a favicon of
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = '_static/gluon_s2.png'
+html_favicon = '_static/favicon.png'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
