f771f07161fcfa85ae1304d8bf0eb936737d8991
Aston Zhang <22279212+astonzhang@users.noreply.github.com>
2020-07-31 13:28:57 -0700

    add tf instructions

--- d2l/__init__.py
+++ d2l/__init__.py
@@ -1,9 +1,10 @@
-"""Saved source codes for "Dive into Deep Learing" (https://d2l.ai).
+"""Saved source code for "Dive into Deep Learing" (https://d2l.ai).
 
 Please import d2l by one of the following ways:
 
 from d2l import mxnet as d2l  # Use MXNet as the backend
 from d2l import torch as d2l  # Use PyTorch as the backend
+from d2l import tensorflow as d2l  # Use TensorFlow as the backend
 
 """
 
