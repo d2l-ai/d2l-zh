7122b2ef9708cce94b4046989434971e2cd21f88
Aston Zhang <22279212+astonzhang@users.noreply.github.com>
2021-03-18 14:13:29 -0700

    Update conf.py

--- build/conf.py
+++ build/conf.py
@@ -144,7 +144,7 @@ html_theme_options = {
     'primary_color': 'blue',
     'accent_color': 'deep_orange',
     'header_links' : [
-        ('伯克利深度学习课程', 'https://courses.d2l.ai/berkeley-stat-157/index.html', True, 'fas fa-user-graduate'),
+        ('在线课程直播', 'https://courses.d2l.ai/zh-v2/', True, 'fas fa-user-graduate'),
         ('PDF', 'https://zh.d2l.ai/d2l-zh.pdf', True, 'fas fa-file-pdf'),
         ('Jupyter 记事本文件', 'https://zh.d2l.ai/d2l-zh.zip', True, 'fas fa-download'),
         ('讨论', 'https://discuss.gluon.ai/c/lecture?order=views', True, 'fab fa-discourse'),
