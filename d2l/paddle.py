d389fd56e4d766f4957799c4034f2875475f1da9
w5688414 <w5688414@gmail.com>
2022-05-03 08:59:51 +0000

    Update sgd

--- d2l/paddle.py
+++ d2l/paddle.py
@@ -144,9 +144,9 @@ def sgd(params, lr, batch_size):
 
     Defined in :numref:`sec_linear_scratch`"""
     with paddle.no_grad():
-        for i,param in enumerate(params):
-            param -= lr * params[i].grad/ batch_size
-            params[i].set_value(param.numpy())
+        for i, param in enumerate(params):
+            param -= lr * params[i].grad / batch_size
+            params[i].set_value(param)
             params[i].clear_gradient()
 
 def load_array(data_arrays, batch_size, is_train=True):
