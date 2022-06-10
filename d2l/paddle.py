5fd724d7fd6f612736ec45b62bbdefd052f0b863
w5688414 <w5688414@gmail.com>
2022-06-10 07:18:57 +0000

    Fix arange dtype bug

--- d2l/paddle.py
+++ d2l/paddle.py
@@ -1300,8 +1300,8 @@ def show_trace_2d(f, results):
     Defined in :numref:`subsec_gd-learningrate`"""
     d2l.set_figsize()
     d2l.plt.plot(*zip(*results), '-o', color='#ff7f0e')
-    x1, x2 = d2l.meshgrid(d2l.arange(-5.5, 1.0, 0.1),
-                          d2l.arange(-3.0, 1.0, 0.1))
+    x1, x2 = d2l.meshgrid(d2l.arange(-5.5, 1.0, 0.1, dtype='float32'),
+                          d2l.arange(-3.0, 1.0, 0.1, dtype='float32'))
     d2l.plt.contour(x1, x2, f(x1, x2), colors='#1f77b4')
     d2l.plt.xlabel('x1')
     d2l.plt.ylabel('x2')
